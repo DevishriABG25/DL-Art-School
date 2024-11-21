e475ec5cddec0ecb3c69e1730d533188c6c55fdd
Devi-shri <devishri.balasundaram@accordbgroup.com>
2024-11-21 18:08:39 +0530

    Update symbols.py

--- codes/models/audio/tts/tacotron2/text/symbols.py
+++ codes/models/audio/tts/tacotron2/text/symbols.py
@@ -9,7 +9,7 @@ from models.audio.tts.tacotron2.text import cmudict
 _pad        = '_'
 _punctuation = '!\'(),.:;? '
 _special = '-'
-_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzäöüßÄÖÜأ ب ت ث ج ح خ د ذ ر ز س ش ص ض ط ظ ع غ ف ق ك ل م ن هـ و ي'
+_letters = 'أ ب ت ث ج ح خ د ذ ر ز س ش ص ض ط ظ ع غ ف ق ك ل م ن هـ و ي'
 
 # Prepend "@" to ARPAbet symbols to ensure uniqueness (some are the same as uppercase letters):
 _arpabet = ['@' + s for s in cmudict.valid_symbols]
